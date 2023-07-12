f64ab4b7eee94d55efc3553e5f4fb9e729fec033
JenMora <jeniffermourah73@gmail.com>
2023-07-12 07:54:31 +0000

    learning about creating variables in Python, replacing the second variable with the first variable and concatenating variables

---
+++ variables/variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# create the favorite_food variable
+favorite_food = "steak"
+print(favorite_food)
+
+# create the food variable
+food = "pizza"
+
+# assign food to favorite_food
+favorite_food = food
+
+# print the favorite_food variable
+print(favorite_food)
