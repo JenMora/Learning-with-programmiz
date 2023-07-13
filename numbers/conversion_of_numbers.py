9301d36f390d80e3067ec76592b20a19036dd53f
JenMora <jeniffermourah73@gmail.com>
2023-07-13 06:00:35 +0000

    Remember you cannnot convert complex numbers in python

---
+++ numbers/conversion_of_numbers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#convert from int to float:
+x = float(1)
+
+#convert from float to int:
+y = int(2.8)
+
+#convert from int to complex:
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
